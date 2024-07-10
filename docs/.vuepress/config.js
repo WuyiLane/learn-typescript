26a3d82141630e9947257fa45e499d83d4ff40c7
zuxiangyun <52056350+WuyiLane@users.noreply.github.com>
2024-07-10 14:21:50 +0800

    测试提交louis

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,45 @@
+module.exports = {
+    title: 'TypeScript4 文档',
+    description: 'TypeScript4 最新官方文档翻译',
+    base: '/learn-typescript/',
+    theme: 'reco',
+    locales: {
+        '/': {
+          lang: 'zh-CN'
+        }
+    },
+    themeConfig: {
+        // lastUpdated: '上次更新',
+        subSidebar: 'auto',
+        nav: [
+            { text: '首页', link: '/' },
+            { 
+                text: '冴羽的 JavaScript 博客', 
+                items: [
+                    { text: 'Github', link: 'https://github.com/mqyqingfeng' },
+                    { text: '掘金', link: 'https://juejin.cn/user/712139234359182/posts' }
+                ]
+            }
+        ],
+        sidebar: [
+            {
+                title: '欢迎学习',
+                path: '/',
+                collapsable: false,
+                children: [
+                    { title: "学前必读", path: "/" }
+                ]
+            },
+            {
+              title: "基础学习",
+              path: '/pages/folder1/test1',
+              collapsable: false,
+              children: [
+                { title: "条件类型", path: "/pages/folder1/test2" },
+                { title: "泛型", path: "/pages/folder1/test1" },
+                { title: "接口", path: "/pages/folder1/test3" }
+              ],
+            }
+          ]
+    }
+}
